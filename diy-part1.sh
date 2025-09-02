74d1f207854cdb64cebc2232c6649a97bf3a7d9b
0012h <84757505+0012h@users.noreply.github.com>
2025-09-02 08:58:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,3 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
-# 修改dnsmasq配置
-sed -i 's/dnsmasq /dnsmasq-full /' include/target.mk
